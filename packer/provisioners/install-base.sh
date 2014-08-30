ff0009aac34b04d644303effc057e6a9abe891ee
Nayeem Syed <developerinlondon@gmail.com>
2014-08-30 05:20:37 -0400

    feature(packer): base lxc unit configured

--- packer/provisioners/install-base.sh
+++ packer/provisioners/install-base.sh
@@ -6,11 +6,6 @@ set -o xtrace
 # turn off interactive mode
 DEBIAN_FRONTEND=noninteractive
 
-# disable ssh usedns
-if [ -f /etc/ssh/sshd_config ]; then
-  sudo sed -i "s/#UseDNS yes/UseDNS no/" /etc/ssh/sshd_config
-fi
-
 sudo apt-get install -y --no-install-recommends software-properties-common
 
 # install ansible

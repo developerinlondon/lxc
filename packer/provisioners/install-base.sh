9cc6c438a8904a37821f78af0f06e7b641d2b4e3
Nayeem Syed <developerinlondon@gmail.com>
2014-09-05 21:00:49 -0400

    * turned off strict host key checking * changed default ansible hosts to be localhost

--- packer/provisioners/install-base.sh
+++ packer/provisioners/install-base.sh
@@ -12,3 +12,7 @@ sudo apt-get install -y --no-install-recommends software-properties-common
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
 sudo apt-get install -y --no-install-recommends ansible
+
+# overwrite the ansible hosts file
+rm -f /etc/ansible/hosts
+echo "localhost" > /etc/ansible/hosts

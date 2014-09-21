20c715a4836c35cdb0d0f7114c1ba31e7f097bfe
Nayeem Syed <developerinlondon@gmail.com>
2014-09-21 09:07:01 +0600

    fixed es settings

--- packer/provisioners/mount-es-ext4.sh
+++ packer/provisioners/mount-es-ext4.sh
@@ -1,12 +1,8 @@
-#! /bin/bash
+#!/bin/bash +x
 
 set -e
 set -o xtrace
 
-sudo sgdisk -Z /dev/xvdf
-sudo sgdisk --new=0:0:-32G -t 1:8300 /dev/xvdf
-sudo mkfs.ext4 /dev/xvdf1
-sudo mkdir -p /mnt/data
-
-sudo /bin/mount /dev/xvdf1 /mnt/data
-sudo /bin/bash -c 'echo "/dev/xvdf1 /mnt/data  ext4\n$(cat /etc/fstab)" > /etc/fstab'
+sudo mv es-mount /etc/init.d/
+sudo chmod +x /etc/init.d/es-mount
+sudo update-rc.d es-mount defaults 00
